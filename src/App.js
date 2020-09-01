4dd52714ef1aff6f9ae5593135f215f912c225bc
Jaspinder Sabharwal <jas@jaspinder.net>
2020-08-31 19:25:36 -0700

    added response for zero return results

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './the_shoppies.png';
+import logo from './logo.png';
 import './App.css';
 import axios from "axios";
 import SearchBar from './components/searchBar';
@@ -36,7 +36,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <SearchBar query={searchQuery}/>
-      {data.query && data.query ? 
+      {data.query && data.query && data.data !== undefined ? 
       <h3 className="query">{`Results for "${data.query}"`}</h3> : null}
         <SearchResults data={data} query={data.query}/>
     </div>
--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -4,9 +4,16 @@ import Movie from './Movie';
 
 function SearchResults(props) {
 
+  if (props.data.data !== undefined) {
+    console.log(props.data)
+
   return props.data.data.map(movie => {
     return <Movie key={Movie.id} {...movie} />;
   });
+
+  } else {
+    return <h3 className="query">{`No results found`}</h3>;
+  }
 }
 
 export default SearchResults;
