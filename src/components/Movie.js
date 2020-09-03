f6e4046a11335c196b8f872d406188b25c122d9d
Jaspinder Sabharwal <jas@jaspinder.net>
2020-09-02 17:01:00 -0700

    Added Nominate button with onclick function

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,41 @@
-import React from "react";
+import React from 'react';
 import { Col, Button } from 'reactstrap';
-import display from '../display.jpg'
+import display from '../display.jpg';
+
 
 function Movie(props) {
+  // console.log(props)
+  const nomination = props.nomination;
+  // const [ nominated, setNominated ] = useState({
+  //   title: null,
+  //   poster: null,
+  //   year: null,  
+  // });
 
-  console.log(props)
+  // const handleSubmit = () => {
+  //   setNominated({
+  //     id: props.imdbID,
+  //     title: props.Title,
+  //     poster: props.Poster,
+  //     year: props.Year,
+  //   });
+  // };
+
   return (
     <div>
       <div className="movie-card">
-        <Col xs={6} >
-          <article className="movie">
+        {/* <Col xs={6} > */}
+          <article className="movie" key={props.imdbID}>
             <img className="movie-thumbnail" src={props.Poster !== "N/A" ? props.Poster : display} alt="Movie" />
             <div className="movie-info">
               <div className="movie-title">{props.Title}</div>
               <div className="movie-year">{props.Year}</div>
             </div>
+            <div>
+              <Button className="nominate-btn" color="secondary" onClick={() => nomination(props)} type="submit">Nominate</Button>
+            </div>
           </article>
-        </Col>
-      </div>
-      <div className="movie-card">
-        <Col xs={6} >
-          <article className="movie-nomination">
-            {/* <img className="movie-thumbnail" src={props.Poster} alt="Movie" />
-            <div className={props.id}>
-              <div className="movie-title">{props.Title}</div>
-              <div className="movie-year">{props.Year}</div>
-            </div> */}
-          </article>
-        </Col>
+        {/* </Col> */}
       </div>
     </div>
   );
