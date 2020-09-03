d2704aa2141893b20e57f9036d30e0a7ab2a8b71
Jaspinder Sabharwal <jas@jaspinder.net>
2020-09-02 17:03:45 -0700

    Added props into variables and inserted condition to show error messages when search results error appears

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './searchResults.css';
 import Movie from './Movie';
 
 function SearchResults(props) {
+  const results = props.data;
+  const nomination = props.nomination;
+  // console.log(props)
 
-  if (props.data.data !== undefined) {
-    console.log(props.data)
+  if (results.error) {
 
-  return props.data.data.map(movie => {
-    return <Movie key={Movie.id} {...movie} />;
-  });
+    return <h4 className="query">{results.error === "Too many results." ? `${results.error} Please narrow your search and try again!` : `${results.error} Please try another search!`}</h4>;
 
   } else {
-    return <h3 className="query">{`No results found`}</h3>;
-  }
-}
+
+    return results.data.map(movie => {
+      return <Movie key={movie.imdbID} {...movie} nomination={nomination} />;
+    });
+  };
+};
 
 export default SearchResults;
